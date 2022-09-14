45fcb5d9a50cf2d8605b578914d36f40d76b5776
Haileab Wolde <womahaileab1428@gmail.com>
2022-09-14 11:01:22 -0700

    tasks

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,64 @@
+/**
+  *_putchar - writes the character c to std
+  *@c: parameter to compare
+  *Return: 1 to 0
+  */
+int _putchar(char c);
+/**
+  *print_alphabet - function to print alphabet
+  *
+  */
+void print_alphabet(void);
+
+/**
+  *print_alphabet_x10 - prints alphabet 10 times
+  *
+  *Return: Always 0;
+  */
+void print_alphabet_x10(void);
+
+/**
+  *_islower - main function
+  *
+  *@c: character to compare
+  *
+  *Return: 1 if true, 0 if false
+  */
+int _islower(int c);
+
+/**
+  *_isalpha - main function
+  *
+  *@c: character to compare
+  *
+  *Return: 1 if true, 0 if false
+  */
+int _isalpha(int c);
+
+/**
+  *print_sign - main function
+  *
+  *@n: character to compare
+  *Return: 1 is positive, 0 if zero, -1 if negative
+  */
+int print_sign(int n);
+
+/**
+  *_abs - main function
+  *
+  *@n:integer to make absolute
+  *Return: int or int times negative
+  */
+int _abs(int n);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
