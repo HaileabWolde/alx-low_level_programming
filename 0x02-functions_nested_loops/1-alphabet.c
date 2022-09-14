bd64e4037f346c6c3931afa2d6cc27f14d01ab26
Haileab Wolde <womahaileab1428@gmail.com>
2022-09-14 11:15:22 -0700

    task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main-check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; a++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
