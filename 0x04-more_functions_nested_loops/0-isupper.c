4e26142762f112a5fd800558decc266722104606
Haileab Wolde <womahaileab1428@gmail.com>
2022-09-16 11:58:29 -0700

    nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main-checks for uppercase character
+ *
+ * Return:1 if it is uppercase else 0
+ */
+int_isupper(int c)
+{
+	while (c >= 'a' || c <= 'z')
+	{
+		Return(0);
+	}
+		Return(1);
+}
