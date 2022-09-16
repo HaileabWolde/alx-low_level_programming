5852957fee98919a3965ebcb2351e2b86089d04e
Haileab Wolde <womahaileab1428@gmail.com>
2022-09-16 13:32:08 -0700

    task

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,8 @@
-#inculde "main.h"
+#include "main.h"
 
 /**
- * print_numbers(void) - prints from 0 to 9
- * Return: Always 0
- */
+  *print_numbers - print 0 to 9
+  */
 
 void print_numbers(void)
 {
