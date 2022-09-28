09672f897c55dade0beeb2bd2670d541f39b3a20
Haileab Wolde <womahaileab1428@gmail.com>
2022-09-28 13:39:56 -0700

    pointers

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,8 +11,16 @@
 
 char *_strchr(char *s, char c)
 {
-	if (strchr(s, c) != null)
-		return (s);
-	else
-		return (NULL);
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	if (s[i] == c)
+		return (s + i);
+	return (0);
 }
