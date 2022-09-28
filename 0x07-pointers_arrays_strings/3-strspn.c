8d5fb8902b61d57ee8af63198febec679ce9f1f9
Haileab Wolde <womahaileab1428@gmail.com>
2022-09-28 13:37:52 -0700

    james bond

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,22 +11,22 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	char i, j, n;
+	int i;
+	int j;
+	unsigned int result = 0;
 
-	n = 0;
-
-	j = 0;
-
-	for (i = 0; *(accept + i) != '\0'; ++i)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; *(s + j) != '\0'; ++j)
+		for (j = 0; s[j] != '\0'; j++)
 		{
-			if (*(accept + i) == *(s + j))
+			if (s[i] == accept[j])
 			{
-				n++;
+				result++;
 				break;
 			}
 		}
+		if (s[j] == '\0')
+			return (result);
 	}
-	return (n + 1);
+	return (result);
 }
