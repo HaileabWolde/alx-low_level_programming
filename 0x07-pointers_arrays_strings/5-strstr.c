2280af4c15ba7b32d1d99ce2a4c1943eaaf9d768
Haileab Wolde <womahaileab1428@gmail.com>
2022-09-30 04:35:47 -0700

    pointers to pointers

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: pointer haystack
+ * @needle: pointer needle
+ *
+ * Description: it locates s2 in s1
+ * Return: if true haystack if false zero
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; needle[i] != '\0'; ++i)
+	{
+		for (j = 0; haystack[j] != '\0'; ++j)
+		{
+			if (needle[i] == haystack[j])
+				return (haystack + j);
+		}
+	}
+	return ('\0');
+}
