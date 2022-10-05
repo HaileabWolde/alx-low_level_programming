a4a23ccc4e7eb6640fc9ffee75ba906a4fc62f9d
Haileab Wolde <womahaileab1428@gmail.com>
2022-10-05 02:54:07 -0700

    my first task on malloc function

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars
+ * @size: integer called size
+ * @c: character c
+ *
+ * Description: it creates an array of chars using dynamic allocation by malloc
+ * Return: NULL if size 0 or pointer N if true
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+
+	char *n;
+
+	n = malloc(sizeof(char) * size);
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	else
+	{
+		for (i = 0; i < size; ++i)
+			n[i] = c;
+
+		return (n);
+	}
+}
