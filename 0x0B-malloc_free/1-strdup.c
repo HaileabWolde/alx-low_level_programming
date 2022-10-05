030f495b990a7fbdc75b15c4f8c8ca145f4c702d
Haileab Wolde <womahaileab1428@gmail.com>
2022-10-05 04:54:44 -0700

    malloc function task 2

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - copies a string
+ * @str: string
+ *
+ * Return: Null if false if true pointer
+ */
+
+char *_strdup(char *str)
+{
+	int l, i;
+
+	char *n;
+
+	for (l = 0; str[l] != '\0'; ++l)
+		;
+
+	n = malloc(sizeof(char) * l + 1);
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		if (n == NULL)
+			return (NULL);
+		
+		for (i = 0; i < l; ++i)
+			n[i] = str[i];
+
+		return (n);
+	}
+}
