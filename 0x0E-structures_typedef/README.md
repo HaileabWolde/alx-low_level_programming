0d8e20faf79990833bd5c7498e7d2765e3028bb4
Haileab Wolde <womahaileab1428@gmail.com>
2022-10-10 05:09:38 -0700

    Struct

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This is a readme file for c on the topic of struct and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure dog
+ * @name: name
+ * @age: age
+ * @owner: char
+ *
+ * Return: nothing
+ */
+
+struct dog
+{
+	char *name;
+
+	float age;
+
+	char *owner;
+};
+#endif
