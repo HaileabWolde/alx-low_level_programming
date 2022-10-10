bc9e3f2e2af91f4707d0c82d3f50f13384929f1a
Haileab Wolde <womahaileab1428@gmail.com>
2022-10-10 13:36:38 -0700

    struct printing what is inside

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - it prints a struct dog
+ * @d: pointer d
+ *
+ * Return: nothing
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		printf("(nill)\n");
+
+	if (d->owner == NULL)
+		printf("(nill)\n");
+
+	printf("Name: %s\nAge: %f\nOwner: %s", d->name, d->age, d->owner);
+}
