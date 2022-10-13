33804645dfe3c6caa95e30dcfbaded29a3b8af35
Haileab Wolde <womahaileab1428@gmail.com>
2022-10-13 03:26:32 -0700

    function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - it is a function that prints a name
+ * @name: pointer whihch holds an address of char
+ * @f: pointer of  a function
+ *
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+this is a readme file for funtcion pointers in C
