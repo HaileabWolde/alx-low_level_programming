089b249ccc8bea44ab31b6d020f7aadf45dcaeee
Haileab Wolde <womahaileab1428@gmail.com>
2022-10-14 08:00:10 -0700

    vardiac function

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,14 +17,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		int x = va_arg(args, int);
-	/*it will pull out the integer value from the undetremined parmater given to it */
-	
+
 		printf("%d", x);
-		
-		if ( separator != NULL && i != n - 1)
+
+		if (separator != NULL && i != n - 1)
 			printf("%s ", separator);
 	}
 	printf("\n");
