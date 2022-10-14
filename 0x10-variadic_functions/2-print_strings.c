b38f2fc1a55cef0052b9410b0dc2b604f193e1d2
Haileab Wolde <womahaileab1428@gmail.com>
2022-10-14 12:26:34 -0700

    vardiac function

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,7 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 				printf("%s", s);
 
 			if (i != n && separator != NULL)
-				printf("%s", separator);
+				printf("%s ", separator);
 		}
 		va_end(valist);
 	}
