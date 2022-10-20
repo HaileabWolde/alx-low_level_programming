720dbe7d348f4f18f2a90c16f64e5df406f37f04
Haileab Wolde <womahaileab1428@gmail.com>
2022-10-20 13:15:19 -0700

    singly linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all of the elements
+ * @h : structure pointer
+ *
+ * Return: unsigned int
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int i, count;
+
+	const list_t *p = NULL;
+
+	count = 0;
+
+	if (h->str == NULL)
+		printf("[0] (nil)");
+	
+	p = h;
+
+	for (i = 0; i < h->len; i++)
+		;
+
+	printf("[%d] %s\n", i, h->str);
+
+	for (i = 0; i < h->len; i++)
+		;
+	printf("[%d] %s\n", i, h->next->str);
+
+	while(p != NULL)
+	{
+		count++;
+		p = p->next;
+	}
+	
+	return (count);
+
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+this is a readme file for a singly linked list in c
