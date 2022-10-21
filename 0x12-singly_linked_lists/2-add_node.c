14face89e80591b55272f733c9cfd7c5ba2ccbc1
Haileab Wolde <womahaileab1428@gmail.com>
2022-10-21 03:37:47 -0700

    adding a node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * add_node - returns the number of elements
+ * @head : pointer to pointer
+ * @str: pointer to char
+ *
+ * Return: head
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t));
+
+
+	if (*(head) == NULL)
+	{
+		*(head) = new;
+
+		new->str = strdup(str);
+
+		new->len = strlen(str);
+
+		if (*(head) == NULL)
+		{
+			return (NULL);
+		}
+
+		return (*(head));
+	}
+
+	new->next = *(head);
+
+	*(head) = new;
+
+	new->str = strdup(str);
+
+	new->len = strlen(str);
+
+	if (*(head) == NULL)
+	{
+		return (NULL);
+	}
+
+	return (*(head));
+}
