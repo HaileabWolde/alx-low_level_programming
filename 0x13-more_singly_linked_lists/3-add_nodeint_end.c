305ff676fa1b1e83ba4be4a0763ab8c3db376de3
Haileab Wolde <womahaileab1428@gmail.com>
2022-10-24 07:43:27 -0700

    singly linked list adding node at the end

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_nodeint_end - add a node at the end
+ * @head: pointer to a pointer
+ * @n: const int
+ *
+ * Return: a pointer if ture if not NULL
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *temp;
+
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+
+	temp = *head;
+
+	if (head == NULL)
+		return (0);
+
+	if (*head == NULL)
+	{
+		*head = new;
+		new->n = n;
+		return (*head);
+	}
+
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	temp->next = new;
+
+	new->next = NULL;
+
+	new->n = n;
+
+	return (*head);
+}
